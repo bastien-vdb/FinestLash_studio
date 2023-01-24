29430a1c44a16087e24927da474c2f786d14a9f4
bastien-vdb <bastien.deboisrolin@gmail.com>
2023-01-24 15:51:44 +0100

    CSS: form changed of prestation2

--- src/components/reusable/Prestations.js
+++ src/components/reusable/Prestations.js
@@ -7,7 +7,7 @@ export default function Prestations({ img, descr, title }) {
         <>
             <div className='flex flex-col items-center justify-center gap-2 my-2'>
                 <div className="border border-[#CCB3AE] border-4 m-6 rounded-full">
-                <Image className='w-44 md:w-40 lg:w-60 rounded rounded-full' src={img} width={657} height={743} alt={descr} />
+                    <Image className='rounded-full w-44 md:w-40 lg:w-60 rounded' src={img} width={657} height={743} alt={descr} />
                 </div>
                 <h3 className=''>{descr}</h3>
                 <h2 className='text-2xl p-0 m-0'>{title}</h2>
