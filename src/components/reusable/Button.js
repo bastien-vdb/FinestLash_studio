710eeb0d0f08af034e19721b9ec8ed7341b73476
bastien-vdb <bastien.deboisrolin@gmail.com>
2023-05-28 22:30:57 +0200

    modification ifram link

--- src/components/reusable/Button.js
+++ src/components/reusable/Button.js
@@ -5,7 +5,7 @@ function Button({ text, link, setPayClicked }) {
     return (
         <>
             <Link href={link ? link : ''}>
-                <button onClick={setPayClicked ? () => setPayClicked(true) : ''} className='border border-[#ccb3ae] px-8 p-2 text-[#ccb3ae] uppercase'>
+                <button onClick={setPayClicked ? ()=>setPayClicked(true): null} className='border border-[#ccb3ae] px-8 p-2 text-[#ccb3ae] uppercase'>
                     {text}
                 </button>
             </Link>
--- src/pages/RendezVous.js
+++ src/pages/RendezVous.js
@@ -1,13 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 function RendezVous(props) {
 
     const [loading, setLoading] = useState(true);
 
+    const ifram = useRef(null);
+
+
     return (
         <div>
             {loading && <div className='loading flex text-4xl text-[#CCB3AE] text-center justify-center items-center h-screen'>Veuillez patienter quelques secondes...</div>}
-            <iframe id="Reservations"
+            {/* <iframe id="Reservations"
                 preload="true"
                 title="Réservations Rendez-vous"
                 width="100%"
@@ -17,6 +20,21 @@ function RendezVous(props) {
                 frameBorder="0"
                 onLoad={() => setLoading(false)}
                 style={{ display: loading ? 'none' : 'block' }}
+            >
+            </iframe> */}
+            <iframe id="Reservations"
+                ref={ifram}
+                preload="true"
+                title="Réservations Rendez-vous"
+                width="100%"
+                height="100%"
+                className='h-screen'
+                src="https://dzt.pgb.mybluehost.me"
+                frameBorder="0"
+                onLoad={() => setLoading(false)}
+                style={{ display: loading ? 'none' : 'block' }}
+                sandbox='allow-same-origin allow-scripts'
+                allowSameOrigin 
             >
             </iframe>
         </div>
