3d047991418a6deafd86e2d9209f5a2ead47df2e
bastien-vdb <bastien.deboisrolin@gmail.com>
2023-06-29 18:34:37 +0200

    getback to CSR rendering

--- src/components/Techniques.js
+++ src/components/Techniques.js
@@ -2,34 +2,32 @@ import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 
-const Techniques = ({presta, title, descr, img, price, link, complements, setPayClicked }) => {
-
-    console.log('presta', presta)
+const Techniques = ({ title, descr, img, price, link, complements, setPayClicked }) => {
 
     return (
         <>
             <div className="bg-gray-50 shadow-2xl rounded-lg py-4 flex flex-col justify-between items-center">
                 <div className='h-full'>
-                    <p className="text-xl text-center font-bold text-[#EBACA2]">{presta.title}</p>
-                    <p className='my-6 mx-10 text-justify'>{presta.content}</p>
+                    <p className="text-xl text-center font-bold text-[#EBACA2]">{title}</p>
+                    <p className='my-6 mx-10 text-justify'>{descr}</p>
                 </div>
 
-                {/* {complements &&
+                {complements &&
                     <ul className='mb-10'>
                         <li>- {complements.title1}</li>
                         <li>- {complements.title2}</li>
                         <li>- {complements.title3}</li>
                     </ul>
-                } */}
+                }
 
-                {/* <Image src={img} width={351} height={351} alt={title} />
+                <Image src={img} width={351} height={351} alt={title} />
                 <p className="text-center py-8">
-                    <span className="text-3xl font-bold text-[#EBACA2]">{presta.price}</span>
+                    <span className="text-3xl font-bold text-[#EBACA2]">{price}</span>
                     <span className="text-xs uppercase text-gray-500">€</span>
                 </p>
                 <div className="flex items-center justify-center mt-6">
-                    {/* <Link href={link} className="bg-[#CE6A6B] hover:bg-white hover:border hover:border-[#CE6A6B] px-8 py-2 text-sm text-gray-200 uppercase rounded font-bold transition duration-150" title="Réserver"><button onClick={() => setPayClicked(true)}>Réserver</button></Link> */}
-                {/* </div> */} 
+                    <Link href={link} className="bg-[#CE6A6B] hover:bg-white hover:border hover:border-[#CE6A6B] px-8 py-2 text-sm text-gray-200 uppercase rounded font-bold transition duration-150" title="Réserver"><button onClick={() => setPayClicked(true)}>Réserver</button></Link>
+                </div>
             </div>
         </>
     );
