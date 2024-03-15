f2a9202243f004d09595b40861e6754ecc87730c
bastien-vdb <bastien.deboisrolin@gmail.com>
2024-03-15 05:33:01 +0100

    fix/prod src

--- src/pages/RendezVous.js
+++ src/pages/RendezVous.js
@@ -11,6 +11,8 @@ function RendezVous(props) {
       {/* <div className="mt-32 flex justify-center items-center bg-[#ccb3aed9]">
         <ModalAlert />
       </div> */}
+      {/* src="https://www.quickreserve.app/integrate/65eb8cc33db57d21b3e9c4f7" */}
+      {/* src="http://localhost:3000/integrate/65eb8cc33db57d21b3e9c4f7" */}
       <iframe
         id="Reservations"
         preload="true"
@@ -18,7 +20,7 @@ function RendezVous(props) {
         width="100%"
         // height="100%"
         className="h-screen"
-        src="https://quickreserve.app/integrate/65eb8cc33db57d21b3e9c4f7"
+        src="https://www.quickreserve.app/integrate/65eb8cc33db57d21b3e9c4f7"
         frameBorder="0"
         onLoad={() => setLoading(false)}
         style={{ display: loading ? "none" : "block" }}
@@ -30,7 +32,8 @@ function RendezVous(props) {
           <div className="loading flex text-4xl text-[#CCB3AE] text-center justify-center items-center mt-10">Veuillez patienter quelques secondes...</div>
           <Loader />
         </div>
-      )};
+      )}
+      ;
     </div>
   );
 }
